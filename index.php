fc211ffb2e839d2888d9ab4ad3a18ff97368f5ee
Sandupa Abeywardana <sandupa.isum@gmail.com>
2022-12-04 23:44:42 +0530

    manager and fav add

--- index.php
+++ index.php
@@ -5,6 +5,25 @@
 		<meta lang="en">
 		<meta charset='utf-8'>
 		<meta http-equiv='X-UA-Compatible' content='IE=edge'>
+
+		<link rel="apple-touch-icon" sizes="57x57" href="image/fav/apple-icon-57x57.png">
+		<link rel="apple-touch-icon" sizes="60x60" href="image/fav/apple-icon-60x60.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="image/fav/apple-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="image/fav/apple-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="image/fav/apple-icon-114x114.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="image/fav/apple-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="image/fav/apple-icon-144x144.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="image/fav/apple-icon-152x152.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="image/fav/apple-icon-180x180.png">
+		<link rel="icon" type="image/png" sizes="192x192"  href="image/fav/android-icon-192x192.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="image/fav/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="96x96" href="image/fav/favicon-96x96.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="image/fav/favicon-16x16.png">
+		<link rel="manifest" href="image/fav/manifest.json">
+		<meta name="msapplication-TileColor" content="#ffffff">
+		<meta name="msapplication-TileImage" content="image/fav/ms-icon-144x144.png">
+		<meta name="theme-color" content="#ffffff">
+
 		<title>Cavalee | Restaurant Management System</title>
 		<style>
 			@font-face {
@@ -135,8 +154,8 @@
 			<br>
 			<img src="image/Logo-White.png" class="logo">
 			<h1>Restaurant Management System</h1>
-			<a id="pos" href="staff">POS Portal</a>
-			<a id="mang" class="cstmls" href="admin">Manager Portal</a>
+			<a id="pos" href="pos">POS Portal</a>
+			<a id="mang" class="cstmls" href="manager">Manager Portal</a>
 			<a id="ad" href="admin">Admin Portal</a>
 		</div>
 
